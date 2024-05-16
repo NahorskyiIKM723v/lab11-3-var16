70714f22eaee09ada1038362a1795d4ea3697727
igor <igor@DESKTOP-EH4IM96>
2024-05-16 15:55:12 +0300

    Добавьте файлы проекта.

---
+++ lab11(3)(1)/lab11(3)(1).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int countOnes(int* arr, int size) {
+    int count = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == 1) {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n;
+    cout << "Введіть розмір масиву: ";
+    cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; ++i) {
+        cout << "Введіть елемент " << i << ": ";
+        cin >> arr[i];
+    }
+    int onesCount = countOnes(arr, n);
+    cout << "Кількість елементів дорівнює 1: " << onesCount << endl;
+    delete[] arr;
+
+    return 0;
+}
